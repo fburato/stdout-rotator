ae4c62131c86ab13a656ab20ef547a4e032213a6
Francesco Burato <francesco.burato@gmail.com>
2023-10-29 11:52:59 +0000

    Complete rotation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use log::{self, LevelFilter};
+use log::{self, debug, error, info, LevelFilter};
 use log4rs::append::console::ConsoleAppender;
-use log4rs::append::file;
 use log4rs::config::{Appender, Root};
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::Config;
+use parse_size::parse_size;
 use regex::Regex;
 use std::fmt::Display;
 use std::fs::{self, File};
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Seek, Write};
 use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::sync::mpsc::{self, Receiver, Sender};
@@ -25,14 +25,20 @@ const LOGGER: &str = "rotator";
 struct Args {
     #[arg(long, default_value = "output.log")]
     output_file: String,
-    #[arg(long, default_value = ".")]
-    rotation_directory: String,
     #[arg(short, long, default_value_t = false)]
     gunzip: bool,
+    #[arg(long, default_value = None)]
+    rotation_directory: Option<String>,
     #[arg(short, long, default_value_t = 5)]
     max_history: u32,
     #[arg(long, default_value = None)]
     log_config: Option<String>,
+    #[arg(long, default_value = "50MB", value_parser = file_size)]
+    max_size: u64,
+}
+
+fn file_size(size: &str) -> Result<u64, String> {
+    parse_size(size).map_err(|op| format!("Error while parsing size: {}", op.to_string()))
 }
 
 #[derive(Debug)]
@@ -68,8 +74,8 @@ struct RotationResult {
 impl RotationResult {
     fn new(existing_rotated: Vec<PathBuf>, next_rotation: PathBuf) -> RotationResult {
         RotationResult {
-            existing_rotated: existing_rotated,
-            next_rotation: next_rotation,
+            existing_rotated,
+            next_rotation,
         }
     }
 }
@@ -78,22 +84,34 @@ fn start_stdout_writing(rxstdout: Receiver<Vec<u8>>, txcomplete: Sender<bool>) -
     thread::spawn(move || {
         let mut stdout = io::stdout();
         let mut stop = false;
+        let logger = "stdout_writer";
         while !stop {
             let read_result = rxstdout.recv();
-            if read_result.is_err() {
+            if let Err(result) = read_result {
                 stop = true;
+                error!(target: logger, "Erro while reading result: {}", result.to_string());
                 continue;
             }
             let read = read_result.unwrap();
-            stdout.write(&read).unwrap();
-            txcomplete.send(true).unwrap();
+            if let Err(result) = stdout.write(&read) {
+                stop = true;
+                error!(target: logger, "Error while writing result: {}", result.to_string());
+                continue;
+            }
+            if let Err(result) = txcomplete.send(true) {
+                stop = true;
+                error!(target: logger, "Error while sending acknowledgment: {}", result.to_string());
+            }
         }
     })
 }
 
 fn start_file_writing(
+    max_history: u32,
+    max_size: u64,
+    compress: bool,
     output: &str,
-    rotation_directory: &str,
+    rotation_directory: Option<&str>,
     rxfile: Receiver<Vec<u8>>,
     txcomplete: Sender<bool>,
 ) -> Result<JoinHandle<()>, RotatorError> {
@@ -119,20 +137,6 @@ fn start_file_writing(
                 op.to_string()
             )
         })?;
-    let result = next_file(output, rotation_directory)?;
-    let gunzip: File = File::options()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(result.next_rotation)
-        .map_err(|op| {
-            format!(
-                "Error during opening of target file '{}', {}",
-                output,
-                op.to_string()
-            )
-        })?;
-    let mut compressor = GzEncoder::new(gunzip, Compression::default());
     file.set_len(0).map_err(|op| {
         format!(
             "Error during truncate of file '{}', {}",
@@ -140,24 +144,140 @@ fn start_file_writing(
             op.to_string()
         )
     })?;
+    let rotation_copy = rotation_directory.map(|s| s.to_string());
+    let output_copy = output.to_string();
     let handle = thread::spawn(move || {
         let mut stop: bool = false;
+        let logger = "file_writer";
         while !stop {
             let read_result = rxfile.recv();
-            if read_result.is_err() {
+            if let Err(result) = read_result {
                 stop = true;
+                error!(target: logger, "Error while reading result: {}", result.to_string());
                 continue;
             }
             let read = read_result.unwrap();
-            file.write_all(&read).unwrap();
-            compressor.write_all(&read).unwrap();
-            txcomplete.send(true).unwrap();
+            let write = file.write_all(&read);
+            if let Err(result) = write {
+                stop = true;
+                error!(target: logger, "Error while writing result to file: {}", result.to_string());
+                continue;
+            }
+            let rotation_result = perform_rotation(
+                &mut file,
+                max_history,
+                max_size,
+                compress,
+                &output_copy,
+                rotation_copy.as_ref().map(|s| s.as_str()),
+            );
+            if let Err(result) = rotation_result {
+                stop = true;
+                error!(target: logger, "Error while rotating file: {}", result.to_string());
+                continue;
+            }
+            if let Err(result) = txcomplete.send(true) {
+                stop = true;
+                error!(target: logger, "Error while sending confirmation: {}", result.to_string());
+            }
         }
-        let mut finish = compressor.finish().unwrap();
-        finish.flush().unwrap();
-        file.flush().unwrap();
+        if let Err(result) = file.flush() {
+            error!(target: "file_writer", "Error while flushing file: {}", result.to_string());
+        }
     });
     Ok(handle)
+}
+
+fn perform_rotation(
+    current_file: &mut File,
+    max_history: u32,
+    max_size: u64,
+    compress: bool,
+    output_file: &str,
+    rotation_directory: Option<&str>,
+) -> Result<(), RotatorError> {
+    let current_position = current_file.stream_position().unwrap();
+    if current_position <= max_size {
+        return Ok(());
+    }
+    info!(target: LOGGER, "File size reached {} bytes, rotating", current_position);
+    let rotation_result = next_file(compress, output_file, rotation_directory)?;
+    if max_history == 0 {
+        cleanup_rotations(max_history, &rotation_result)?;
+        current_file
+            .set_len(0)
+            .map_err(|op| format!("Error while truncating {}: {}", output_file, op.to_string()))?;
+        current_file.seek(io::SeekFrom::Start(0)).map_err(|op| {
+            format!(
+                "Error while seeking to beginning of {}: {}",
+                output_file,
+                op.to_string()
+            )
+        })?;
+        return Ok(());
+    }
+    cleanup_rotations(max_history - 1, &rotation_result)?;
+    current_file
+        .flush()
+        .map_err(|op| format!("Error while flushing {}: {}", output_file, op.to_string()))?;
+    current_file.seek(io::SeekFrom::Start(0)).map_err(|op| {
+        format!(
+            "Error while seeking to beginning of {}: {}",
+            output_file,
+            op.to_string()
+        )
+    })?;
+    let mut target: File = File::options()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&rotation_result.next_rotation)
+        .map_err(|op| {
+            format!(
+                "Error during opening of target file '{}', {}",
+                &rotation_result.next_rotation.display(),
+                op.to_string()
+            )
+        })?;
+    if compress {
+        let mut compressor = GzEncoder::new(target, Compression::default());
+        io::copy(current_file, &mut compressor).map_err(|op| {
+            format!(
+                "Error while copying {} to {} during compression: {}",
+                output_file,
+                &rotation_result.next_rotation.display(),
+                op.to_string()
+            )
+        })?;
+        compressor
+            .finish()
+            .map_err(|op| format!("Error while finishing compression: {}", op.to_string()))?
+            .flush()
+            .map_err(|op| format!("Error while flushing compressed file: {}", op.to_string()))?;
+    } else {
+        io::copy(current_file, &mut target).map_err(|op| {
+            format!(
+                "Error while copying {} to {}: {}",
+                output_file,
+                &rotation_result.next_rotation.display(),
+                op.to_string()
+            )
+        })?;
+        target
+            .flush()
+            .map_err(|op| format!("Error while flushing file: {}", op.to_string()))?;
+    }
+    current_file
+        .set_len(0)
+        .map_err(|op| format!("Error while truncating {}: {}", output_file, op.to_string()))?;
+    current_file.seek(io::SeekFrom::Start(0)).map_err(|op| {
+        format!(
+            "Error while seeking to beginning of {}: {}",
+            output_file,
+            op.to_string()
+        )
+    })?;
+    Ok(())
 }
 
 fn start_read_cycle(
@@ -244,13 +364,20 @@ fn config_logger(maybe_config: &Option<String>) -> Result<(), RotatorError> {
     }
 }
 
-fn next_file(output_file: &str, rotation_directory: &str) -> Result<RotationResult, RotatorError> {
+fn next_file(
+    compression: bool,
+    output_file: &str,
+    rotation_directory: Option<&str>,
+) -> Result<RotationResult, RotatorError> {
     let base_path = PathBuf::from(output_file);
-    let parent = if rotation_directory.to_string() == "" {
-        ".".to_string()
-    } else {
-        rotation_directory.to_string()
-    };
+    let base_parent = base_path
+        .parent()
+        .unwrap()
+        .to_path_buf()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let parent = rotation_directory.unwrap_or(&base_parent);
     log::debug!(target: LOGGER, "parent={}", &parent);
     let paths = fs::read_dir(&parent).map_err(|op| {
         format!(
@@ -261,7 +388,11 @@ fn next_file(output_file: &str, rotation_directory: &str) -> Result<RotationResu
     })?;
     let mut maximum = 0;
     let base_name = base_path.file_name().unwrap().to_str().unwrap();
-    let pattern = format!("^{}\\.(?<digit>[0-9]+)\\.gz$", base_name);
+    let pattern = if compression {
+        format!("^{}\\.(?<digit>[0-9]+)\\.gz$", base_name)
+    } else {
+        format!("^{}\\.(?<digit>[0-9]+)$", base_name)
+    };
     let path_regex = Regex::new(&pattern).unwrap();
     let mut existing_rotated: Vec<(i32, PathBuf)> = vec![];
     log::debug!(target: LOGGER, "pattern={}", &path_regex);
@@ -278,7 +409,7 @@ fn next_file(output_file: &str, rotation_directory: &str) -> Result<RotationResu
         if let Some(capture) = path_regex.captures(&file_name) {
             let digit = &capture["digit"];
             let parsed = digit.parse::<i32>().unwrap();
-            let mut existing_buffer = PathBuf::from(rotation_directory);
+            let mut existing_buffer = PathBuf::from(&parent);
             existing_buffer.push(file_name);
             existing_rotated.push((parsed, existing_buffer));
             if maximum <= parsed {
@@ -287,20 +418,50 @@ fn next_file(output_file: &str, rotation_directory: &str) -> Result<RotationResu
         }
     }
     existing_rotated.sort_by(|(d1, _), (d2, _)| d1.cmp(d2));
-    let mut output_path = PathBuf::from(rotation_directory);
-    output_path.push(format!("{}.{}.gz", base_name, (maximum + 1)));
+    let mut output_path = PathBuf::from(&parent);
+    let path = if compression {
+        format!("{}.{}.gz", base_name, (maximum + 1))
+    } else {
+        format!("{}.{}", base_name, (maximum + 1))
+    };
+    output_path.push(path);
     let existing_rotated: Vec<PathBuf> = existing_rotated
         .iter()
-        .map(move |(_, path)| path.to_owned())
+        .map(|(_, path)| path.to_owned())
         .collect();
     log::debug!(target: LOGGER, "next_file={}, existing={:?}", &output_path.display(), &existing_rotated);
     Ok(RotationResult::new(existing_rotated, output_path))
 }
 
+fn cleanup_rotations(max_files: u32, rotation_result: &RotationResult) -> Result<(), RotatorError> {
+    if rotation_result.existing_rotated.len() > max_files.try_into().unwrap() {
+        let to_remove: u32 =
+            u32::try_from(rotation_result.existing_rotated.len()).unwrap() - max_files;
+        for i in 0..to_remove {
+            let file_to_clean = &rotation_result.existing_rotated[usize::try_from(i).unwrap()];
+            debug!(target: LOGGER, "Removing '{}'", file_to_clean.display());
+            fs::remove_file(file_to_clean).map_err(|op| {
+                format!(
+                    "Error while removing '{}': {}",
+                    file_to_clean.display(),
+                    op.to_string()
+                )
+            })?;
+        }
+    }
+    Ok(())
+}
+
 fn app(args: Args) -> Result<(), RotatorError> {
     config_logger(&args.log_config)?;
     log::info!(target: LOGGER, "Parsed command line arguments: {:?}", args);
-    log::debug!(target: LOGGER, "Test debug");
+    log::debug!(target: LOGGER, "Cleaning up rotations");
+    let rotation_result = next_file(
+        args.gunzip,
+        &args.output_file,
+        args.rotation_directory.as_ref().map(|s| s.as_str()),
+    )?;
+    cleanup_rotations(args.max_history, &rotation_result)?;
     let (txstdout, rxstdout) = mpsc::channel::<Vec<u8>>();
     let (txfile, rxfile) = mpsc::channel::<Vec<u8>>();
     let (txcomplete1, rxcomplete) = mpsc::channel::<bool>();
@@ -309,8 +470,11 @@ fn app(args: Args) -> Result<(), RotatorError> {
     let stdout_handle = start_stdout_writing(rxstdout, txcomplete1);
     log::info!(target: LOGGER, "Starting file writing");
     let file_handle = start_file_writing(
+        args.max_history,
+        args.max_size,
+        args.gunzip,
         &args.output_file,
-        &args.rotation_directory,
+        args.rotation_directory.as_ref().map(|s| s.as_str()),
         rxfile,
         txcomplete2,
     )?;
